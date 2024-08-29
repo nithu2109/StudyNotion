6d9e5607b1086302cff540092a24b9a398b7eb8a
vagu21 <bharadwaj21197@gmail.com>
2024-08-29 11:10:44 +0530

    Created Models

---
+++ SERVER/models/Course.js
@@ -0,0 +1,49 @@
+const mongoose=require('mongoose')
+
+const courseSchema=new mongoose.Schema({
+ courseName:{
+    type:String,
+ },
+ courseDescription:{
+    type:String,
+ },
+ instructor:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+ },
+ whatyouWillLearn:{
+    type:String,
+ },
+ courseContent:[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Section",
+    }
+ ],
+ ratingAndReviews:[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"RatingAndReview",
+
+    }
+ ],
+ Price:{
+    type:Number,
+
+ },
+ thumbnail:{
+        type:String,
+ },
+ tag:{
+    type:mongoose.Schema.Types.ObjectId,
+        ref:"Tag",
+
+ },
+ studentsEnrolled:{
+    type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+ }
+})
+
+module.exports=mongoose.model("Course",courseSchema)
